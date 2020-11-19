7e20649a2cc749b355e81f9c439e9a8681996282
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-11-19 18:37:26 +0200

    Small command line ui to Main

--- src/main/java/nextbook/Main.java
+++ src/main/java/nextbook/Main.java
@@ -1,9 +1,27 @@
 package nextbook;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give 'add' to add book");
+        System.out.println("Give empty line to exit program");
+        System.out.println("");
+
+        while (true) {
+            String command = sc.nextLine();
+            if (command.isEmpty()) {
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.println("Book added");
+            }
+            System.out.println("");
+        }
+
     }
 
 }
