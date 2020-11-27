b3fc8e2127bad602ec65b99d3fc53700a7ac8e15
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-11-27 10:44:06 +0200

    Add Video, new parameters in command line add

---
+++ src/main/java/nextbook/domain/Video.java
@@ -0,0 +1,14 @@
+public class Video {
+
+    private String name;
+    private String link;
+    private int time;
+
+    public Video(String name, String link, int time) {
+        this.name = name;
+        this.link = link;
+        this.time = time;
+    }
+
+}
+
--- src/main/java/nextbook/ui/Ui.java
+++ src/main/java/nextbook/ui/Ui.java
@@ -18,7 +18,8 @@ public class Ui {
     }
 
     public void start() {
-        System.out.println("Give 'add' to add book");
+        System.out.println("Give 'add book' to add book");
+        System.out.println("Give 'add video' to add video");
         System.out.println("Give 'list' to list books");
         System.out.println("Give empty line to exit program");
 
@@ -29,12 +30,32 @@ public class Ui {
                 break;
             }
 
-            if (command.equals("add")) {
+            if (command.equals("add book")) {
                 System.out.println("Give name of the book");
                 String name = sc.nextLine();
                 System.out.println("Give author of the book");
                 String author = sc.nextLine();
+                System.out.println("Give ISBN of the book");
+                String isbn = sc.nextLine();
+                System.out.println("Give year of publish of the book");
+                int year = Integer.parseInt(sc.nextLine());
+                System.out.println("Give comments to the book (all in one line)");
+                String comment = sc.nextLine();
+
                 Clue book = new Book(name, author);
+                clueService.createClue(book);
+                System.out.println("New book added");
+            }
+
+            if (command.equals("add video")) {
+                System.out.println("Give name of the video");
+                String name = sc.nextLine();
+                System.out.println("Give link to the video");
+                String link = sc.nextLine();
+                System.out.println("Give starting time of part of the video (in seconds)");
+                int startTime= Integer.parseInt(sc.nextLine());
+
+                Clue book = new Video(name, link, startTime);
                 clueService.createClue(book);
                 System.out.println("New book added");
             }
