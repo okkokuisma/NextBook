71639eca43024d1d7a2e727db144af4522cc99f3
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-12-04 10:27:01 +0200

    Add class tag

---
+++ src/main/java/nextbook/domain/Tag.java
@@ -0,0 +1,25 @@
+package nextbook.domain;
+
+public class Tag {
+
+    private String tag;
+
+    public Tag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public String toString() {
+        return "tag: " + tag;
+    }
+
+}
+
