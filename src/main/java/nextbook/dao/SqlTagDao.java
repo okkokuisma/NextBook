76b3370e8bef8508b7098bdd2a762d03aa13a990
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-12-06 10:10:10 +0200

    Tag delete sql implementation

--- src/main/java/nextbook/dao/SqlTagDao.java
+++ src/main/java/nextbook/dao/SqlTagDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nextbook.dao;
 
 import java.sql.Connection;
@@ -18,10 +13,6 @@ import nextbook.domain.Clue;
 import nextbook.domain.Tag;
 import nextbook.domain.Video;
 
-/**
- *
- * @author okkokuisma
- */
 public class SqlTagDao {
     private Connection dbconn;
     private DbUtil dbUtil;
@@ -29,33 +20,33 @@ public class SqlTagDao {
     public SqlTagDao(DbUtil dbUtil) {
         this.dbUtil = dbUtil;
     }  
-    
+
     public void create(Tag tag) {
         try {
             connect();
             PreparedStatement ps = dbconn.prepareStatement("INSERT INTO tags (tag) VALUES (?)");
             ps.setString(1, tag.getTag());
             ps.execute();
-            
+
             dbconn.close();
         } catch (SQLException ex) {
             Logger.getLogger(SqlBookDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public ArrayList getAll() {
         try {
             connect();
             Statement ps = dbconn.createStatement();
             ResultSet queryResults = ps.executeQuery("SELECT id, tag FROM tags");
-            
+
             ArrayList<Tag> tags = new ArrayList<>();
             while (queryResults.next()) {
                 Tag tag = new Tag(queryResults.getString(2));
                 tag.setId(queryResults.getInt(1));
                 tags.add(tag);
             }
-            
+
             dbconn.close();
             return tags;
         } catch (SQLException ex) {
@@ -63,12 +54,12 @@ public class SqlTagDao {
             return null;
         }     
     }
-    
+
     public void setTag(Clue clue, Tag tag) {
         try {
             connect();
             PreparedStatement ps = dbconn.prepareStatement("");
-            
+
             if (clue instanceof Book) {
                 ps = dbconn.prepareStatement("INSERT INTO book_tags (book_id, tag_id) VALUES (?,?)");
                 ps.setInt(1, ((Book) clue).getId());
@@ -76,33 +67,51 @@ public class SqlTagDao {
                 ps = dbconn.prepareStatement("INSERT INTO video_tags (video_id, tag_id) VALUES (?,?)");
                 ps.setInt(1, ((Video) clue).getId());
             }
-            
+
             ps.setInt(2, tag.getId());
             ps.execute();
-            
+
             dbconn.close();
         } catch (SQLException ex) {
             Logger.getLogger(SqlBookDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    public void deleteTag(int id) {
+        try {
+            connect();
+
+            PreparedStatement ps1 = dbconn.prepareStatement("DELETE FROM book_tags, video_tags WHERE tag_id = ?");
+            ps1.setInt(1, id);
+            ps1.execute();
+
+            PreparedStatement ps2 = dbconn.prepareStatement("DELETE FROM tags WHERE id = ?");
+            ps2.setInt(1, id);
+            ps2.execute();
+
+            dbconn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(SqlBookDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public ArrayList filterAllByTag(Tag tag) {
         ArrayList<Clue> clues = new ArrayList<>();
         clues.addAll(filterBooksByTag(tag));
         clues.addAll(filterVideosByTag(tag));
-        
+
         return clues;
     }
-    
+
     private ArrayList filterBooksByTag(Tag tag) {
         try {
             connect();
             PreparedStatement ps = dbconn.prepareStatement("SELECT id, name, author, isbn, comment, year FROM books WHERE id IN (SELECT book_id FROM book_tags WHERE tag_id = ?)");
             ps.setInt(1, tag.getId());
             ResultSet queryResults = ps.executeQuery();
-            
+
             ArrayList<Book> books = SqlBookDao.getQueryResultAsListOfClues(queryResults);
-            
+
             dbconn.close();
             return books;
         } catch (SQLException ex) {
@@ -110,7 +119,7 @@ public class SqlTagDao {
             return null;
         } 
     }
-    
+
     private ArrayList filterVideosByTag(Tag tag) {
         try {
             connect();
@@ -126,7 +135,7 @@ public class SqlTagDao {
             return null;
         } 
     }
-    
+
     private void connect() {
         dbconn = dbUtil.connect();
     }
